d3d02438b4fed363fd8d5fcf4a43ea6ff083ab47
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-15 16:47:56 +0200

    mysqldatabase-cmds

--- clarify.sh
+++ clarify.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-dnf install mysql-server -y &>>$LOGFILE
+dnf install mysql-server -y 
 echo $? "Installing MySQL Server"
 
-systemctl enable mysqld &>>$LOGFILE
+systemctl enable mysqld 
 echo $? "Enabling MySQL Server"
 
-systemctl start mysqld &>>$LOGFILE
+systemctl start mysqld 
 echo $? "Starting MySQL Server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 
