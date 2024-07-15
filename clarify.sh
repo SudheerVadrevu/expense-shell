6692af9b69b63b24b5c121358fb29dc8cdb36a95
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-15 16:45:25 +0200

    mysqldatabase-cmds

--- clarify.sh
+++ clarify.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
-
 mysql_secure_installation --set-root-pass ExpenseApp@1 
-VALIDATE $? "Setting up root password"
+echo $? "Setting up root password"
